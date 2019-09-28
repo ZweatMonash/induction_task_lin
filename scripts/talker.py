a562f7f757ee33c84ff2c0fbbf179737045f3a81
ZweatMonash <zlzwe1@student.monash.edu>
2019-09-28 15:11:18 +1000

    added comments. modify subscriber to reject bad turtle quality

--- scripts/talker.py
+++ scripts/talker.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 # license removed for brevity
+
+"""
+<One sentence description>.
+
+Author: Zwe Lin Htet
+
+Date last updated: 28/9/2019 by Zwe Lin Htet
+
+Purpose: Publish turtle information
+
+Subscribed topic/s: 
+    _______
+    _______
+
+ Published topic/s:
+    /induction_task_lin/turtle_talk
+
+"""
+
 import rospy
 import numpy as np
 from std_msgs.msg import String
-from induction_task_lin.msg import turtle_info
+from induction_task_lin.msg import TurtleInfo
 
 def talker():
-    pub = rospy.Publisher('/turtle_talk', turtle_info, queue_size=10)
+    pub = rospy.Publisher('/induction_task_lin/turtle_talk', TurtleInfo, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(5) #  1 Hz
+    rate = rospy.Rate(5) #  5 Hz
     index = 0
-    info_of_turtle = turtle_info()
+    info_of_turtle = TurtleInfo()
     while not rospy.is_shutdown():
 
         info_of_turtle.index = index
--- src/listener.cpp
+++ src/listener.cpp
@@ -1,21 +1,41 @@
+/*
+ *--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
+ * <Subscribe to turtle information from talker.py>. 
+ *
+ * Author: Zwe Lin Htet
+ *
+ * Date last updated: 28/9/2019 by Zwe Lin Htet
+ *
+ * Purpose: receipt of turtle information and check if turtle quality
+ *          meets the criteria
+ * 
+ * Subscribed topic/s: 
+ *     /induction_task_lin/turtle_talk
+ 
+ *--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "induction_task_lin/turtle_info.h"
+#include "induction_task_lin/TurtleInfo.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const induction_task_lin::turtle_info::ConstPtr& info_of_turtle)
+
+void chatterCallback(const induction_task_lin::TurtleInfo::ConstPtr& info_of_turtle)
 {
-  ROS_INFO("Turtle index:  [%d] \nTurtle quality: [%d] \n ",info_of_turtle->index \
+  if(info_of_turtle->quality_of_turtle >= 7){
+  ROS_INFO("Turtle index:  [%d] \t Turtle quality: [%d] \n ",info_of_turtle->index \
 							   ,info_of_turtle->quality_of_turtle);
+  }else{
+  ROS_INFO("Turtle index:  [%d] \t Turtle quality does not meet the criteria");
+  }
+
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/turtle_talk", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("/induction_task_lin/turtle_talk", 1000, chatterCallback);
   ros::spin();
 
   return 0;
